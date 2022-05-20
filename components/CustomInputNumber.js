1a67f54f3680af83dc54b4bfc9677f5ed1feabf4
regchiu <regx1385@gmail.com>
2022-05-20 12:00:57 +0800

    feat: CustomInputNumber

---
+++ components/CustomInputNumber.js
@@ -0,0 +1,131 @@
+import React, { useState, useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import './CustomInputNumber.css'
+
+function CustomInputNumber({ min, max, step = 1, value = 0, name, disabled, onChange, onBlur }) {
+  const inputRef = useRef(null)
+  const [innerValue, setInnerValue] = useState(value)
+  const timer = useRef(null)
+
+  useEffect(() => {
+    onChange({
+      target: {
+        name: inputRef.current.name,
+        value: inputRef.current.value,
+      },
+    })
+  })
+
+  function increase() {
+    setInnerValue((prev) => {
+      if (prev + step > max) {
+        clearTimer()
+        return max
+      } else {
+        return prev + step
+      }
+    })
+  }
+
+  function decrease() {
+    setInnerValue((prev) => {
+      if (prev - step < min) {
+        clearTimer()
+        return min
+      } else {
+        return prev - step
+      }
+    })
+  }
+
+  function handleMouseDown(operate) {
+    const start = Date.now()
+    switch (operate) {
+      case '+':
+        timer.current = setInterval(() => {
+          const end = Date.now()
+          if (end - start > 500) {
+            increase()
+          }
+        }, 100)
+        break
+      case '-':
+        timer.current = setInterval(() => {
+          const end = Date.now()
+          if (end - start > 500) {
+            decrease()
+          }
+        }, 100)
+        break
+      default:
+        break
+    }
+  }
+
+  function clearTimer() {
+    clearInterval(timer.current)
+  }
+
+  function handleChange(event) {
+    event.target.value = Number(event.target.value)
+    setInnerValue(Number(event.target.value))
+  }
+
+  function handleBlur(event) {
+    if (innerValue > max || innerValue < min) {
+      setInnerValue(value)
+    }
+    onBlur(event)
+  }
+
+  return (
+    <div className="custom-input-number">
+      <button
+        className="btn"
+        disabled={disabled}
+        onClick={() => decrease()}
+        onMouseDown={() => handleMouseDown('-')}
+        onMouseLeave={() => clearTimer()}
+        onMouseUp={() => clearTimer()}
+      >
+        -
+      </button>
+      <input
+        ref={inputRef}
+        className="input-number"
+        type="number"
+        min={min}
+        max={max}
+        step={step}
+        name={name}
+        value={innerValue}
+        disabled={disabled}
+        onChange={(event) => handleChange(event)}
+        onBlur={(event) => handleBlur(event)}
+      />
+      <button
+        className="btn"
+        disabled={disabled}
+        onClick={() => increase()}
+        onMouseDown={() => handleMouseDown('+')}
+        onMouseLeave={() => clearTimer()}
+        onMouseUp={() => clearTimer()}
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
+CustomInputNumber.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number,
+  name: PropTypes.string,
+  value: PropTypes.number,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+}
+
+export default CustomInputNumber
--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import CustomInputNumber from '../components/CustomInputNumber'
 
 function App() {
+  function onChange(event) {
+    console.log('onChange name', event.target.name)
+    console.log('onChange value', event.target.value)
+  }
+
+  function onBlur(event) {
+    console.log('onBlur name', event.target.name)
+    console.log('onBlur value', event.target.value)
+  }
+
   return (
     <div>
-      <h2>Welcome to React App</h2>
-      <h3>Date : {new Date().toDateString()}</h3>
+      <CustomInputNumber
+        name={'customInputNumber'}
+        min={0}
+        max={10}
+        value={0}
+        step={3}
+        onChange={(event) => onChange(event)}
+        onBlur={(event) => onBlur(event)}
+      ></CustomInputNumber>
     </div>
   )
 }
